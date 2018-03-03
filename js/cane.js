4c046a4986aaa360256c1d2012e3630ef756a650
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2018-03-03 10:58:57 +0530

    version-1.0.0 canetrader official website

---
+++ js/cane.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
+  
+  // Slide in elements on scroll
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+
+  $('body').on('click','#myBtn',function(){
+    $("#myModal").show();
+  });
+  // When the user clicks on <span> (x), close the modal
+  document.getElementsByClassName("modal-close")[0].onclick = function() {
+      $("#myModal").hide();
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        $("#myModal").hide();
+    }
+  }
+
+})
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(11.7455502, 78.1325846);
+  var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}
