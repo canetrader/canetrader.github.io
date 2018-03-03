1a36e6da512ee775eaeb785ca53350d57f43a059
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2018-03-03 20:15:17 +0530

    Added content and refactored few sections

--- js/cane.js
+++ js/cane.js
@@ -51,6 +51,9 @@ function myMap() {
   var myCenter = new google.maps.LatLng(11.7455502, 78.1325846);
   var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  var marker = new google.maps.Marker({position:myCenter});
+  var marker = new google.maps.Marker({
+    position:myCenter,
+    title: "caneman"
+  });
   marker.setMap(map);
 }
