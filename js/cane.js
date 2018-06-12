b7412ac3fa4c2e3c2b3b2db907ded8d8337c9aa1
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2018-06-12 13:25:07 +0530

    Added a block for existing service worker

--- js/cane.js
+++ js/cane.js
@@ -52,9 +52,16 @@ function reviewAppend(){
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-  .register('/sw.js')
-  .then(function() {
-    console.log('Service worker registered!');
-  });
+  if(navigator.serviceWorker.controller)
+  {
+    console.log('Active service worker found, no need to register')
+  }
+  else
+  {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
+    });
+  }
 }
