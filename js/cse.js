4cd092dd1327abd133f1905dcd901633626707e3
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2018-06-13 16:57:36 +0530

    Added custom search box

---
+++ js/cse.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '010767885309651231674:nak9lwkf1je';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
